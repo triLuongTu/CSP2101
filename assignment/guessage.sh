89004145f28c27d691915ee2a6ddbb066cd376f0
Tu triLuong <triLuongTu@github.com>
2020-04-09 12:52:51 +0800

    New

--- assignment/guessage.sh
+++ assignment/guessage.sh
@@ -15,11 +15,12 @@
 
 int='^[0-9]+$' #create variable to check input(integer)
 
+#Using while loop to create a input loop until variable is an integer
 while true; do
     #Prompt the player to type min age to create a range of random age number
     read -p "Type the minimum age to create guess age game: " minAge
 
-    #Create if funtion to check variable input(integers only)
+    #Create if function to check variable input(integers only)
     if ! [[ "$minAge" =~ $int ]]; then
         echo "Error: Integer only. Please try again!"
     else
@@ -27,7 +28,7 @@ while true; do
         #Prompt the player to type max age to create a range of random age number
         read -p "Type the maximum age to create guess age game: " maxAge
 
-        #Create if funtion to check variable input(integers only)
+        #Create if function to check variable input(integers only)
         if ! [[ "$maxAge" =~ $int ]]; then
             echo "Error: Integer only. Please try again!"
         else
@@ -42,21 +43,26 @@ while true; do
 done
 
 
-#Use user input to create random age by using shuf
-age=$( shuf -i ${minAge}-${maxAge} -n 1 )
+#Use user input to create random age (by using shuf command)in range minAge-maxAge
+age=$( shuf -i $minAge-$maxAge -n 1 )
 echo "Random age are created!"
 
-
+#Using while loop to create a input loop until variable is an integer
 while true; do
     #Prompt the player to guess age number in the range they choose
-    read -p "Type the age number to guess in range $minAge-$maxAge: " guessAge
-    if ! [[ "$minAge" =~ $int ]]; then
+    read -p "Type the number of age to guess in range $minAge - $maxAge: " guessAge
+    #Create if function to check variable input(integers only, e to exit)
+    if [[ "$guessAge" = "e" ]]; then
+        echo "The age is $age years old. Good bye!"
+        break
+    elif ! [[ "$guessAge" =~ $int ]]; then
         echo "Error: Age must be an integer. Please try again!"
     else
+        #Create if function to compare variable input(guessAge) with random age
         if [[ $guessAge -gt $age ]]; then
-            echo "Too high. Try again!"
+            echo "Too high. Try again!(Press e to exit)"
         elif [[ $guessAge -lt $age ]]; then
-            echo "Too low. Try again!"
+            echo "Too low. Try again!(Press e to exit)"
         else
             echo "Correct. The age is $age years old."
             break
